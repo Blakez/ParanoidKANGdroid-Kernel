290c46fbbfccb0a4c5b939729eaa2c956cc26dd8
Blake Zimmerman <blake.zimmerman.sjp@gmail.com>
2013-03-21 17:15:19 -0400

    Old kernel wouldn't build, this should fix it
    Now to re-apply my commits….

--- arch/arm/mach-msm/hotplug.c
+++ arch/arm/mach-msm/hotplug.c
@@ -22,7 +22,6 @@
 #include "pm.h"
 #include "spm.h"
 
-#ifdef CONFIG_SMP
 extern volatile int pen_release;
 
 struct msm_hotplug_device {
@@ -68,7 +67,6 @@ static inline void platform_do_lowpower(unsigned int cpu, int *spurious)
 		(*spurious)++;
 	}
 }
-#endif
 
 int platform_cpu_kill(unsigned int cpu)
 {
